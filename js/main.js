135f90721eb905fc82e5fa02ebb49ea6036e0d6e
Marcie Van Auken <marcievanauken@gmail.com>
2015-02-08 11:31:37 -0500

    starting to compile my .js cheatsheet

---
+++ js/main.js
@@ -0,0 +1,88 @@
+
+
+/* alert pop up */
+// alert ("Hello World!");
+
+
+
+
+/* Variable */
+// var x = "8";
+
+// if (x>=7)
+// {
+// console.log("bigger than 10")
+// }
+// else{
+// console.log("less than 10")
+// }
+
+
+
+
+
+/* here's a function */
+// function doorPrize(doorNumber){
+// 	if(doorNumber == 1){
+// 		console.log("you won big");
+
+// 	}else if(doorNumber == 2){
+// 		console.log("you won nothing");
+
+// 	}else{
+// 		console.log("stop that");
+// 	}
+// }
+
+// var door = window.prompt("choose a door")
+
+// doorPrize(door);
+
+
+
+
+/* Variables */
+var taco = 4;
+var burrito = 6;
+console.log(taco+burrito)
+
+
+
+/* Variables */
+// var taco = "cat";
+// var burrito = "6";
+// console.log(taco+burrito)
+
+
+
+
+/* here's a function */
+// alert(4+6)
+
+
+
+/* Array */
+// var myArray = [["cat","tiger"],["dog","wolf"]]
+
+// console.log(myArray[0]);
+
+// console.log(myArray[1][0]);
+
+// console.log(myArray[1][1]);
+
+
+
+
+/* Debugging */
+// var x = "red";
+
+// if (x === "red")
+// {
+// console.log("The color was red")
+// }
+// else{
+// console.log("nope")
+// }
+
+
+
